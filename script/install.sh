76bd2e956ea170ed688620dbe2eb5937095681db
@slovacus <slovacus@gmail.com>
2017-09-10 00:49:32 -0500

    !#14 Fix: settings script for makefile

--- script/install.sh
+++ script/install.sh
@@ -7,11 +7,11 @@
 if [ "$1" == "" ]; then
     pip install -r requirements.txt
 elif [ "$1" == "dev" ]; then
-    pip install -r "${REQUIREMENTS_DIR}/"dev.txt
+    pip install -r "${REQUIREMENTS_DIR}/dev.txt"
 elif [ "$1" == "stage" ]; then
-    pip install -r "${REQUIREMENTS_DIR}/"stage.txt
+    pip install -r "${REQUIREMENTS_DIR}/stage.txt"
 elif [ "$1" == "test" ]; then
-    pip install -r "${REQUIREMENTS_DIR}/"test.txt
+    pip install -r "${REQUIREMENTS_DIR}/test.txt"
 elif [ "$1" == "prod" ]; then
     pip install -r requirements.txt
 fi
