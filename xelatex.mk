6fda8410ec7f329c9b38c930754e5f5d4aed4ab0
@slovacus <slovacus@gmail.com>
2017-10-16 02:17:53 -0500

    !#27 Add: action for make latex resume

--- xelatex.mk
+++ xelatex.mk
@@ -6,4 +6,4 @@ DOCKER_RUN := docker-compose -f docker-compose.yml run --rm app
 
 resume.make: clean
 	$(DOCKER_RUN) \
-		bash -c "cd src && xelatex resume.tex";
+		bash -c "cd src && xelatex -output-directory=_build main.tex";
