94ac593fac5d854384fbf99ff62a2aa267d72a33
@slovacus <slovacus@gmail.com>
2021-03-04 19:55:27 -0500

    refactor: delete unnecessary cv (#76)

--- provision/make/xelatex.mk
+++ provision/make/xelatex.mk
@@ -4,26 +4,20 @@
 CC = xelatex
 EXAMPLES_DIR = examples
 RESUME_DIR = examples/resume
-CV_DIR = examples/cv
 
 xelatex.help:
 	@echo '    xelatex:'
 	@echo ''
 	@echo '        xelatex          show help'
-	@echo '        xelatex.make     make resume and cv'
+	@echo '        xelatex.make     make resume'
 	@echo '        xelatex.resume   make resume'
-	@echo '        xelatex.cv       make cv'
 	@echo ''
 
 xelatex:
 	make xelatex.help
 
-xelatex.make: xelatex.resume xelatex.cv
+xelatex.make: xelatex.resume
 
 xelatex.resume:
 	@$(docker-dev-run) \
 		bash -c "cd src && $(CC) -output-directory=$(EXAMPLES_DIR) resume.tex";
-
-xelatex.cv:
-	@$(docker-dev-run) \
-		bash -c "cd src && $(CC) -output-directory=$(EXAMPLES_DIR) cv.tex";
