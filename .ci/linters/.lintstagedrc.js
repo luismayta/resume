d5d45a29c36b33e1e45c665afb123e8054e63cc7
Luis Mayta <luis@hadenlabs.com>
2026-03-25 05:58:48 -0500

    ci 👷 (ci): update linting and pre-commit config (#170)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- .ci/linters/.lintstagedrc.js
+++ .ci/linters/.lintstagedrc.js
@@ -1,8 +1,8 @@
-const escape = require("shell-quote").quote
+const shellQuote = require("shell-quote").quote
 
 module.exports = {
   "*.{ts,tsx,js,json,css}": (filenames) => [
-    ...filenames.map((filename) => `prettier --check "${escape([filename])}"`),
+    ...filenames.map((filename) => `prettier --check "${shellQuote([filename])}"`),
     ...filenames.map((filename) => `git add "${filename}"`)
   ],
   "*.{ts,tsx,js,jsx}": ["eslint"],
